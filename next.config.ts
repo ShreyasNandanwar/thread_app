13cc6a9403e38543f44bb7d9fdd05fe1b1edb909
[Shreyas Nandanwar] <shreyasnandanwar0400@gmail.com>
2024-10-30 23:40:35 +0530

    First Commit

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+﻿import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["img.clerk.com", "images.clerk.dev", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      }
+    ],
+  },
+};
+
+export default nextConfig;
+
